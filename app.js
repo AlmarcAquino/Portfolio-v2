bb79ae0a0f8b6b45fcb8c5058ae7552ed04ecce1
AlmarcAquino <almarcfaquino@gmail.com>
2022-07-08 02:16:45 -0700

    Initial Commit - landing page created

---
+++ app.js
@@ -0,0 +1,10 @@
+// async function firstSixIncomplete() {
+//     const promise = await fetch("https://jsonplaceholder.typicode.com/todos");
+//     const result = await promise.json();
+    
+//     const filteredResults = result.filter(elem => !elem.completed).slice(0,6);
+//     console.log(filteredResults);
+
+// }
+
+// firstSixIncomplete();
